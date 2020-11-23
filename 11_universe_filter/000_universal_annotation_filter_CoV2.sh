308e5a35a94a13906a2b4e20f598bb275e79d056
jhyouk <jhyouk@gmail.com>
2020-11-23 15:29:05 +0900

    201123

--- 11_universe_filter/000_universal_annotation_filter_CoV2.sh
+++ 11_universe_filter/000_universal_annotation_filter_CoV2.sh
@@ -5,7 +5,7 @@ mttype="$2" # snp or indel
 reference="$3" # mm10 or hg19
 somaticbam="$4"
 germlinebam="$5"
-panelofnormal="$6" #../31_1_PanelOfNormal_SPark/mm10_b6_4_190226.4s.q0Q0.chr1.mpileup.indel.edit.gz
+#panelofnormal="$6" #../31_1_PanelOfNormal_SPark/mm10_b6_4_190226.4s.q0Q0.chr1.mpileup.indel.edit.gz
 #below 2 args are optional input for invitro 
 #motherbam="$7"
 #motherID="$8"
@@ -13,6 +13,7 @@ panelofnormal="$6" #../31_1_PanelOfNormal_SPark/mm10_b6_4_190226.4s.q0Q0.chr1.mp
 case $3 in
 	mm10)	species="mouse";;
 	hg19)	species="human";;
+	cov2)	species="cov2";;
 esac
 case $2 in
 	snp)	temp_mttype="snv";;
@@ -24,23 +25,20 @@ outDir=$(dirname $1)
 log=$outDir/$1.$2.annot.log
 
 echo $1 $2 $3 $4 $5 > $log
-if false; then
 echo varscan somatic filtering >> $log
 (python /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/00_varscan_somaticfilter.py ../05_varscan/$1.varscan.$2.vcf) &>> $log || { c=$?;echo "Error";exit $c; }
 echo done >> $log
 echo "start: union of pass call in varscan2 somatic & strelka2" >>$log
 (python /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/00_vcf_combination_by_Youk_$2.py $1 $species 2 ../02_Strelka2/$1/results/variants/somatic."$temp_mttype"s.vcf.gz ../05_varscan/$1.varscan.$2.somatic.vcf) &>> $log || { c=$?;echo "Error";exit $c; }
 echo "done" >>$log
-fi
-
 echo "initial annotation" >> $log
-(sh /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/sypark_PointMt_annot_filter/PointMt_annot.sh $1_$2_union_2.vcf $4 $5 /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/sypark_PointMt_annot_filter/src $3) &>> $log || { c=$?;echo "Error";exit $c; }
+(sh /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/sypark_PointMt_annot_filter/PointMt_annot_cov2.sh $1_$2_union_2.vcf $4 $5 /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/sypark_PointMt_annot_filter/src $3) &>> $log || { c=$?;echo "Error";exit $c; }
 echo "done" >>$log
-echo "panel of normal annotation" >> $log
-(python /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/02_AddNpanelToVCF_"$2".py $1_$2_union_2.readinfo.readc.rasmy.vcf $6 PanelofNormal $species) &>> $log || { c=$?;echo "Error";exit $c; }
-echo "done" >>$log
-echo "filter1 using sample and germline information"  >>$log
-(python /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/03_$2_filter1.py $1_$2_union_2.readinfo.readc.rasmy_PanelofNormal.vcf) &>> $log || { c=$?;echo "Error";exit $c; } #b6
+#echo "panel of normal annotation" >> $log
+#(python /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/02_AddNpanelToVCF_"$2".py $1_$2_union_2.readinfo.readc.rasmy.vcf $6 PanelofNormal $species) &>> $log || { c=$?;echo "Error";exit $c; }
+#echo "done" >>$log
+#echo "filter1 using sample and germline information"  >>$log
+#(python /home/users/jhyouk/81_filter_test_LADC/11_universe_filter/03_$2_filter1.py $1_$2_union_2.readinfo.readc.rasmy_PanelofNormal.vcf) &>> $log || { c=$?;echo "Error";exit $c; } #b6
 
 #from here, invitro culture only"
 #echo "annotation of mother cell info"
